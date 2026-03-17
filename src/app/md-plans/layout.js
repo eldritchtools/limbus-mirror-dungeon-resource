6cf5fd2386eef9020a5aa7c994f9cfa2477363ca
eldritchtools <eldritch.tools@gmail.com>
2026-03-17 14:48:13 +0800

    Fix a build error

--- src/app/md-plans/layout.js
+++ src/app/md-plans/layout.js
@@ -1,8 +1,12 @@
+import { Suspense } from "react";
+
 export const metadata = {
     title: "MD Plans | Limbus Company Mirror Dungeon Resource",
     description: "MD Plans"
 };
 
 export default function MdPlansLayout({ children }) {
-    return <>{children}</>
+    return <Suspense fallback={<div>Loading...</div>}>
+        {children}
+    </Suspense>
 }
